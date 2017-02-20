afd172182fb45cc4c1c732e133776600fe6eb818
brandon-schabel <brandonschabel1995@gmail.com>
2017-02-20 12:24:16 -0800

    Initial commit

---
+++ src/components/Post/AllPosts.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                    class="list-group-item"
+                    v-for="index in 5">
+                Server #{{ index }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    data
+    export default {
+        data() {
+            return {
+                posts: []
+            };
+        },
+        methods: {
+            fetchData() {
+                this.$http.get('/getallpostsapi')
+                .then(response => {
+                    return response.json();
+                    
+                }).then(data => {
+                    const resultArray = [];
+                    for(let key in data) {
+                        resultArray.push(data[key]);
+                    }
+                    this.posts = resultArray;
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Post/Post.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul class="list-group">
+        <li class="list-group-item" v-for="u in users">{{u.username}} - {{u.email}} </li>
+    </ul>
+</template>
+
+<script>
+
+</script>
+<!-- if we want the styling to only apply to one component we add
+sciped to it -->
+<style scoped>
+    export default {
+        <ul class="list-group">
+                    <li class="list-group-item" v-for="u in users">{{u.username}} - {{u.email}} </li>
+                </ul>
+    }
+
+</style>
